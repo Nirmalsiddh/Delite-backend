6e13ae7caa78afaa52fe218d038e23d8d2a1d678
Nirmalsiddh <nirmalsiddh2812@gmail.com>
2025-07-08 16:39:24 +0530

    Second commit

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,13 @@ import noteRoutes from './routes/noteRoutes';
 dotenv.config();
 const app = express();
 
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   credentials: true
+// }));
 app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
+  origin: ['http://localhost:3000', 'https://your-vercel-project-name.vercel.app'],
+  credentials: true,
 }));
 app.use(express.json());
 
