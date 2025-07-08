9b5a8275c597da319fdecfb403152882b81e2b79
Nirmalsiddh <nirmalsiddh2812@gmail.com>
2025-07-08 16:00:56 +0530

    first commit

--- src/server.ts
+++ src/server.ts
@@ -40,6 +40,8 @@ app.get('/', (req, res) => {
 });
 
 // start server
-app.listen(process.env.PORT, () =>
-  console.log(`Server running at http://localhost:${process.env.PORT}`)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
 );
